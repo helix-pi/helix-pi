9c4a0a936e5aa1506e0fa59baefec2738978e7b0
Nick Johnstone <ncwjohnstone@gmail.com>
2015-07-12 18:39:37 +1200

    Serializer now checks deserialized entities work (hint: they don't)

--- test/serializer.js
+++ test/serializer.js
@@ -1,6 +1,8 @@
 const {serialize, deserialize} = require('../app/serializer');
 const Seeder = require('../app/seeder');
 const createApi = require('../app/api');
+const simulateWorld = require('../app/simulator');
+const Entity = require('../app/entity');
 
 const assert = require('assert');
 
@@ -17,8 +19,11 @@ describe("serialization", () => {
     const individual = Seeder.make(stubApi, 1)[0];
 
     const serializedIndividual = serialize(individual);
+    const deserializedIndividual = deserialize(serializedIndividual);
 
     assert.equal(deserialize(serializedIndividual).length, individual.length);
     assert.equal(typeof deserialize(serializedIndividual)[0], "function");
+
+    simulateWorld([new Entity(deserializedIndividual, {x: 0, y: 0}, [], true)], 10, []);
   });
 });
