e1b1668cecbd0f5e90b0d337440e887c95ccfdf4
Nick Johnstone <ncwjohnstone@gmail.com>
2015-07-19 16:45:10 +1200

    Turns out the Object.assign constructor lines up perfectly with reduce. funny that

--- app/pretty-print.js
+++ app/pretty-print.js
@@ -17,7 +17,7 @@ function prettyArguments (args) {
 
   return Object.keys(args)
     .map(argument => prettyArgument(argument, args[argument]))
-    .reduce((prettyArgs, argument) => Object.assign(prettyArgs, argument), {});
+    .reduce(Object.assign, {});
 }
 
 function prettyArgument (argumentName, value) {
