9ae0d64612ce1d88f2d39eeae7510e6e6cf9978d
Nick Johnstone <ncwjohnstone@gmail.com>
2017-08-19 19:33:05 +1200

    Hello world!

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { run } from '@cycle/run';
+import { makeDOMDriver, div, DOMSource, VNode } from '@cycle/dom';
+import { timeDriver, TimeSource } from '@cycle/time';
+import xs, { Stream } from 'xstream';
+
+interface ISources {
+  DOM: DOMSource;
+  Time: TimeSource;
+}
+
+interface ISinks {
+  DOM: Stream<VNode>;
+}
+
+function main(sources: ISources): ISinks {
+  return {
+    DOM: xs.of(div('hello world!'))
+  }
+}
+
+const drivers = {
+  DOM: makeDOMDriver(document.body),
+  Time: timeDriver
+};
+
+run(main, drivers);
